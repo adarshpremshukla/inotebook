53123807f3b00287902f2d3cf122079f2373e1e8
Adarsh Shukla <adarshshukla0512@gmail.com>
2024-11-27 13:37:36 +0530

    Update index.js
    changes in index.js

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,9 @@ const connectToMongo = require('./db');
 connectToMongo();
 
 const app = express();
-const port = 3000;
+const port = 5000;
 
-// app.use(express.json());
+app.use(express.json());
 
 // Example route
 app.get('/', (req, res) => {
