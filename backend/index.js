81606154d59943df672590b71495aaa1095fa971
Adarsh Shukla <adarshshukla0512@gmail.com>
2024-11-29 17:17:08 +0530

    video 44
    create schema(Notes.js User.js) and Routes(auth.js notes.js)

--- backend/index.js
+++ backend/index.js
@@ -4,14 +4,18 @@ const connectToMongo = require('./db');
 connectToMongo();
 
 const app = express();
-const port = 5000;
+const port = 3000;
 
 app.use(express.json());
 
 // Example route
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+// app.get('/', (req, res) => {
+//   res.send('Hello, World!');
+// });
+
+// Available Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
