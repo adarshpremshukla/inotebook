36e1785df00905fc1ee62f4365c058014ab9a307
Adarsh Shukla <adarshshukla0512@gmail.com>
2024-11-30 17:43:13 +0530

    video 46
    added express validator for validation and showing error

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const {body, validationResult}= require('express-validator')
 
-// create a user using POST "/api/auth/". doesn't require auth
-router.post('/',(req,res)=>{
-    console.log(req.body);
-    const user = User(req.body);
-    user.save()
-    res.send(req.body)
+// create a user using: POST "/api/auth/". Doesn't require authentication
+router.post('/',[
+    body('name','Enter a valid name').isLength({min:3}),
+    body('email','Enter a valid mail').isEmail(),
+    body('password','Password must be atleast 5 characters').isLength({min:5}),
+],(req,res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()});
+    }
+    User.create({
+        name:req.body.name,
+        password:req.body.password,
+        email:req.body.email,
+        }).then(user => res.json(user))
+        .catch(err => {console.log(err)
+            res.json({error:'please enter a unique value for email', message:err.message})
+        })
 })
 
 module.exports = router
