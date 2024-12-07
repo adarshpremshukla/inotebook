031c2a3ee0fd6469112f62e9bc7cc26263914def
adarshpremshukla <adarshshukla0512@gmail.com>
2024-12-07 17:49:11 +0530

    video 59
    used useLocation Hook in navbar for active the links

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -3,22 +3,10 @@ import NoteContext from "./noteContext";
 
 
 const NoteState = (props) => {
-    const s1 = {
-        "name": "Adarsh",
-        "class": "5b"
-    }
-    const [state, setState] = useState(s1);
-    
-    const update = ()=>{
-        setTimeout(() => {
-            setState({
-                "name": "Rishi",
-                "class": "10b"
-            })
-    }, 1000);
-}
+
+
     return (
-        <NoteContext.Provider value={{state, update}}>
+        <NoteContext.Provider value={{}}>
             {props.children}
         </NoteContext.Provider>
     )
