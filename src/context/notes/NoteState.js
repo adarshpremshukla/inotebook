4d8a5caad0e2471bf8054055c626b9a45f8a7033
adarshpremshukla <adarshshukla0512@gmail.com>
2024-12-08 12:28:56 +0530

    video 66
    adding a modal for editing a note

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -53,7 +53,17 @@ const NoteState = (props) => {
 
 
     //delete a node
-    const deleteNote = (id) => {
+    const deleteNote = async(id) => {
+        //API CALL
+        const response = await fetch(`${host}/api/notes/deletenote/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': "application/json",
+                "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjc0YzUwMGQzZWUxMDA4ZTBhYTQ4MmUxIn0sImlhdCI6MTczMzA2NTQxMn0.4VizFEc9sMvqjZgMlV--LSYMbEAnoBDeQPb76I4BAHI"
+            },
+        });
+        const json =  response.json();
+        console.log(json)
         // to do api call
         const newNotes = notes.filter((note) => { return note._id !== id })
         setNotes(newNotes)
